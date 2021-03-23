c89a537945ee2718b9c5ce7309ee75ef7089fbfa
DingZhenyuan <ding_zy@outlook.com>
2021-03-23 17:45:24 +0800

    add

---
+++ Problems/Array_27_Remove Element.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+}
+
+// better solution
+/*
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            // reduce array size by one
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+}
+*/

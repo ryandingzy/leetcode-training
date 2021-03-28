b6b054b10dd04bfc9bf42722dc8158b05e234f27
DingZhenyuan <ding_zy@outlook.com>
2021-03-28 17:26:52 +0800

    hash in place

---
+++ Problems/0041_First Missing Positive.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if (nums.length == 0)
+            return 1;
+        int length = nums.length;
+        int temp;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] > 0 && nums[i] <= length && nums[nums[i] - 1] != nums[i]) {
+                temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return length + 1;
+    }
+}

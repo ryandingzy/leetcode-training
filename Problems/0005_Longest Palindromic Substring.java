baa57be9c250cea8f73ee33507babd98dfd1b42f
DingZhenyuan <ding_zy@outlook.com>
2021-08-01 11:11:09 +0800

    Dynamic Programming & Expand Around Center & Manacher's Alogrithm

---
+++ Problems/0005_Longest Palindromic Substring.java
@@ -0,0 +1,110 @@
+// dynamic programming
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len == 1)
+            return s;
+        
+        boolean[][] ps = new boolean[len][len];
+
+        for (int i = 0; i < len; i++) {
+            ps[i][i] = true;
+            if (i+1 < len)
+                ps[i][i+1] = s.charAt(i) == s.charAt(i + 1);
+        }
+        for (int j = 2; j < len; j++) {
+            for (int i = 0; i + j < len; i++) {
+                ps[i][i+j] = ps[i+1][i+j-1] && (s.charAt(i) == s.charAt(i+j));
+            }
+        }
+        
+        for (int j = len - 1; j >= 0; j--) {
+            for (int i = 0; i + j < len; i++) {
+                if (ps[i][i+j]) {
+                    return s.substring(i, i+j+1);
+                }
+            }
+        }
+        return s.substring(0, 1);
+    }
+}
+
+// Expand Around Center
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+}
+
+// Manacher's Alogrithm
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0, end = -1;
+        StringBuffer t = new StringBuffer("#");
+        for (int i = 0; i < s.length(); ++i) {
+            t.append(s.charAt(i));
+            t.append('#');
+        }
+        t.append('#');
+        s = t.toString();
+
+        List<Integer> arm_len = new ArrayList<Integer>();
+        int right = -1, j = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            int cur_arm_len;
+            if (right >= i) {
+                int i_sym = j * 2 - i;
+                int min_arm_len = Math.min(arm_len.get(i_sym), right - i);
+                cur_arm_len = expand(s, i - min_arm_len, i + min_arm_len);
+            } else {
+                cur_arm_len = expand(s, i, i);
+            }
+            arm_len.add(cur_arm_len);
+            if (i + cur_arm_len > right) {
+                j = i;
+                right = i + cur_arm_len;
+            }
+            if (cur_arm_len * 2 + 1 > end - start) {
+                start = i - cur_arm_len;
+                end = i + cur_arm_len;
+            }
+        }
+
+        StringBuffer ans = new StringBuffer();
+        for (int i = start; i <= end; ++i) {
+            if (s.charAt(i) != '#') {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+
+    public int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return (right - left - 2) / 2;
+    }
+}

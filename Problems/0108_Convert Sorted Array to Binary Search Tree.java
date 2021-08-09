25656569f27e1ea4c0ee3e122bba28974befd886
DingZhenyuan <ding_zy@outlook.com>
2021-08-09 18:49:14 +0800

    BST & recursion

---
+++ Problems/0108_Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+    
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (end < start) return null;
+        TreeNode root = new TreeNode();
+        if (end == start) {
+            root.val = nums[start];
+            return root;
+        }
+        int mid = (start + end) / 2;
+        root.val = nums[mid];
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
+    }
+}

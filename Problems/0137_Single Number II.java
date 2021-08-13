1aefca20bcb12563b5700b43110d21960efe75c4
DingZhenyuan <ding_zy@outlook.com>
2021-08-13 17:39:42 +0800

    digital circuit logic

---
+++ Problems/0137_Single Number II.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0, b = 0;
+        for (int num : nums) {
+            int aNext = (~a & b & num) | (a & ~b & ~num), bNext = ~a & (b ^ num);
+            a = aNext;
+            b = bNext;
+        }
+        return b;
+    }
+}

c235f8f950653f3498d192af7388c5f20d0cdd6e
DingZhenyuan <ding_zy@outlook.com>
2021-08-13 22:51:22 +0800

    faster and slower pointer

---
+++ Problems/0142_Linked List Cycle II.java
@@ -0,0 +1,58 @@
+// My solution
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode node = head;
+        int count = 1;
+        while (node != null) {
+            ListNode n = head;
+            int i = 1;
+            while (!n.equals(node)) {
+                n = n.next;
+                i++;
+            }
+            if (i < count) return n;
+            
+            node = node.next;
+            count++;
+        }
+        return null;
+    }
+}
+
+// Faster and slower pointer
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}

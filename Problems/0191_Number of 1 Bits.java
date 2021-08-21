77d95bfe2146261d7cbbf70d3b912d3ac342e5c4
DingZhenyuan <ding_zy@outlook.com>
2021-08-21 23:25:48 +0800

    bit operation

---
+++ Problems/0191_Number of 1 Bits.java
@@ -0,0 +1,23 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) count++;
+            n >>>= 1;
+        }
+        return count;
+    }
+}
+
+// better solution
+public class Solution {
+    public int hammingWeight(int n) {
+        int ret = 0;
+        while (n != 0) {
+            n &= n - 1;
+            ret++;
+        }
+        return ret;
+    }
+}

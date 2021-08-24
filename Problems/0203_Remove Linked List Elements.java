12c5bfbf2256925556075239c7e7395ae82fedfc
DingZhenyuan <ding_zy@outlook.com>
2021-08-24 15:27:17 +0800

    linked list

---
+++ Problems/0203_Remove Linked List Elements.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+        ListNode cur = head, pre = newHead;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+                cur = cur.next;
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return newHead.next;
+    }
+}

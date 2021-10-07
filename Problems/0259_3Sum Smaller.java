436f4af3a80d4a5d05fafc3bf71a4ee71026b7de
DingZhenyuan <ding_zy@outlook.com>
2021-10-06 22:30:30 -0700

    binary search & two pointers

---
+++ Problems/0259_3Sum Smaller.java
@@ -0,0 +1,86 @@
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int n = nums.length;
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            for (; j < n - 1; j++) {
+                int k = j + 1;
+                for (; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] < target) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+                if (k <= j + 1) break;
+            }
+            if (j <= i + 1) break;
+        }
+        return count;
+    }
+}
+
+// better solution
+// binary search
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            sum += twoSumSmaller(nums, i + 1, target - nums[i]);
+        }
+        return sum;
+    }
+
+    private int twoSumSmaller(int[] nums, int startIndex, int target) {
+        int sum = 0;
+        for (int i = startIndex; i < nums.length - 1; i++) {
+            int j = binarySearch(nums, i, target - nums[i]);
+            sum += j - i;
+        }
+        return sum;
+    }
+
+    private int binarySearch(int[] nums, int startIndex, int target) {
+        int left = startIndex;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+
+// two pointers
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            sum += twoSumSmaller(nums, i + 1, target - nums[i]);
+        }
+        return sum;
+    }
+
+    private int twoSumSmaller(int[] nums, int startIndex, int target) {
+        int sum = 0;
+        int left = startIndex;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                sum += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return sum;
+    }
+}

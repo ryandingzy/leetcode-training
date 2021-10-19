fcb0cc2bcfab2abf66dc73f8df8aef0c8f93a742
DingZhenyuan <ding_zy@outlook.com>
2021-10-19 16:37:59 -0700

    linked list

---
+++ Problems/0328_Odd Even Linked List.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            ListNode tmp = even.next;
+            even.next = even.next.next;
+            odd.next = tmp;
+            even = even.next;
+            odd = odd.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

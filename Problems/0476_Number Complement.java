7aa8af8f34fcea70bdf640ee03bf0c4377b26b5a
DingZhenyuan <ryandingzy@gmail.com>
2022-01-09 13:54:33 -0800

    bit

---
+++ Problems/0476_Number Complement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findComplement(int num) {
+        int res = 0;
+        int i = 0;
+        while (num != 0) {
+            res |= (1 - (num & 1)) << i;
+            num >>= 1;
+            i++;
+        }
+        return res;
+    }
+}
+
+
+// other method
+class Solution {
+  public int findComplement(int num) {
+    return (Integer.highestOneBit(num) << 1) - num - 1;
+  }
+}

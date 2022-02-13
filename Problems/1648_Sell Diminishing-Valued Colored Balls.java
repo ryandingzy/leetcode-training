18e7433a46c5f2a10bed435775f12504c230d9e3
DingZhenyuan <ryandingzy@gmail.com>
2022-02-13 00:31:19 -0800

    array

---
+++ Problems/1648_Sell Diminishing-Valued Colored Balls.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] inventory, int orders) {
+        long modulo = 1000000007;
+        Arrays.sort(inventory);
+        
+        int cnt = 0, i  = inventory.length - 1;
+        long sum = 0;
+        while (orders > 0) {
+            cnt++;
+            int n = inventory[i];
+            int d = i >= 1? n - inventory[i - 1] : 0; // difference between next ball
+
+            if (d == 0 && i != 0) {
+                i--;
+                continue;
+            }
+
+
+            if (i > 0 && orders >= d * cnt) {
+                sum += (long) (n - d + 1 + n) *d / 2 * cnt;
+                orders -=  d * cnt;
+            } else {
+                int a = orders / cnt;
+                sum += (long) (n - a + 1 + n) * a / 2 * cnt;
+                int b = orders % cnt;
+                sum += (long) (n - a) * b;
+                orders = 0;
+            }
+            sum = (sum + modulo) % modulo;
+            i--;
+        }
+        return (int) sum;     
+    }
+}
